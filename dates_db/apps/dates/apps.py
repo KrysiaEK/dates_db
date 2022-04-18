0e69f12e668998d115102225f307f71a96f9774e
KrysiaEK <kryst.kazanecka@gmail.com>
2022-04-18 23:19:19 +0200

    create dates app

---
+++ dates_db/apps/dates/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DateConfig(AppConfig):
+    name = 'dates_db.apps.dates'
---
+++ dates_db/apps/dates/models.py
@@ -0,0 +1,12 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+from dates_db.apps.dates.utilities import Months
+
+
+class Date(models.Model):
+    """Model representing date."""
+
+    month = models.PositiveSmallIntegerField(choices=Months.Choices)
+    day = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(31)])
+    fact = models.TextField()
